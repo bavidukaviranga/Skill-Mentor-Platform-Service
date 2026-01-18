64d6f6733efbb21465c01ac6ea7ce17bdce2ad74
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-01-18 16:15:12 +0530

    Update SubjectService.java

--- src/main/java/com/stemlink/skillmentor/services/SubjectService.java
+++ src/main/java/com/stemlink/skillmentor/services/SubjectService.java
@@ -1,11 +1,12 @@
 package com.stemlink.skillmentor.services;
 
 import com.stemlink.skillmentor.entities.Subject;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
-
+@Service
 public interface SubjectService {
     List<Subject> getAllSubjects();
     Subject addNewSubject(Long mentorId, Subject subject);
