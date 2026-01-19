a1f27135535cfb6e4a31e7754134d9a5eee31e83
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-01-19 17:12:13 +0530

    create exceptions,dtos and responses

---
+++ src/main/java/com/stemlink/skillmentor/exceptions/SkillMentorException.java
@@ -0,0 +1,15 @@
+package com.stemlink.skillmentor.exceptions;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class SkillMentorException extends RuntimeException{
+    private final HttpStatus status;
+
+    public SkillMentorException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}
