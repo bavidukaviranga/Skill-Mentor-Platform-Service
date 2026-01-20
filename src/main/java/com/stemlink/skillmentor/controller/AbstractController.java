21896d08ff78f86db848ab80e763741fad04fc2f
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-01-20 09:02:18 +0530

    update

--- src/main/java/com/stemlink/skillmentor/controller/AbstractController.java
+++ src/main/java/com/stemlink/skillmentor/controller/AbstractController.java
@@ -1,24 +1,48 @@
 package com.stemlink.skillmentor.controller;
 
+
+import com.stemlink.skillmentor.dto.ErrorResponse;
 import com.stemlink.skillmentor.exceptions.SkillMentorException;
-import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Builder
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 public class AbstractController {
+
+    // reusable methods and logic
+    protected <T> ResponseEntity<T> sendOkResponse(T response) {
+        return ResponseEntity.ok(response);
+    }
+
+    protected <T> ResponseEntity<T> sendCreatedResponse(T response) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    protected <T> ResponseEntity<T> sendNotFoundResponse() {
+        return ResponseEntity.notFound().build();
+    }
+
+    protected <T> ResponseEntity<T> sendNoContentResponse() {
+        return ResponseEntity.noContent().build();
+    }
+
+
     // handle error messages
 
     // common exceptions handling
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(
             Exception ex) {
-
+        log.error("Unexpected error: {}" ,ex.getMessage());
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .message("An unexpected error occurred")
                 .errorCode("INTERNAL SERVER ERROR")
@@ -27,6 +51,7 @@ public class AbstractController {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     //    Handles Custom SkillMentor Exceptions
     @ExceptionHandler(SkillMentorException.class)
     public ResponseEntity<ErrorResponse> handleSkillMentorException(
@@ -41,4 +66,24 @@ public class AbstractController {
         return new ResponseEntity<>(errorResponse, ex.getStatus());
     }
 
+    // Validation Exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("Validation failed")
+                .errorCode("BAD REQUEST")
+                .timestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                .validationErrors(errors)
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/stemlink/skillmentor/dto/ErrorResponse.java
+++ src/main/java/com/stemlink/skillmentor/dto/ErrorResponse.java
@@ -9,21 +9,15 @@ import lombok.NoArgsConstructor;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
+    private String message;
+    private String errorCode;
+    private String timestamp;
 
-
-    @Builder
-    public class ErrorResponse {
-        private String message;
-        private String errorCode;
-        private String timestamp;
-
-        @Builder.Default
-        private Map<String, String> validationErrors = new HashMap<>();
-    }
+    @Builder.Default
+    private Map<String, String> validationErrors = new HashMap<>();
 }
